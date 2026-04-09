646e68db1bfefbf9a41ddc09859d6e8173b9eaf2
dumeojh <dumeojh@gmail.com>
2026-04-10 04:09:07 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,17 @@
 
+// config.js
+
+// 1. 파이어베이스 접속 설정
+const firebaseConfig = {
+    apiKey: "AIzaSyAi_zYqV2PtWNsy61zkxKADxirWftcBfY4",
+    authDomain: "project-94b9c.firebaseapp.com",
+    databaseURL: "https://project-94b9c-default-rtdb.firebaseio.com",
+    projectId: "project-94b9c",
+    storageBucket: "project-94b9c.firebasestorage.app",
+    messagingSenderId: "619238103499",
+    appId: "1:619238103499:web:f25b6406a913954a87fe4e"
+};
+
+// 2. 구글 앱스 스크립트(최종 답안지 제출용) 주소
+// 🚨 "..." 부분에 선생님의 진짜 구글 시트 배포 주소를 꼭 넣어주세요!
+const BASE_SCRIPT_URL = "...";
